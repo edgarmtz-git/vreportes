69db6bbb0ff08268d112b603090b28ff21e84929
frikilabsdev <frikilabs.dev@gmail.com>
2025-12-12 15:37:02 -0600

    fix: simplify vercel.json config for static assets serving

--- client/vite.config.js
+++ client/vite.config.js
@@ -17,10 +17,20 @@ export default defineConfig({
     outDir: "../dist/public",
     emptyOutDir: true,
     assetsDir: "assets",
-    cssCodeSplit: true,
+    cssCodeSplit: false,
     rollupOptions: {
       output: {
-        assetFileNames: "assets/[name]-[hash][extname]",
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.');
+          const ext = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+            return `images/[name]-[hash][extname]`;
+          }
+          if (/css/i.test(ext)) {
+            return `assets/[name]-[hash][extname]`;
+          }
+          return `assets/[name]-[hash][extname]`;
+        },
         chunkFileNames: "assets/[name]-[hash].js",
         entryFileNames: "assets/[name]-[hash].js"
       }
